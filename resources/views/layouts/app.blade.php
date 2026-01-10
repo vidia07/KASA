346bbd94bbe90ba0b98e7e189f6f77edc5c18e65
nekowawolf <irukainu20@gmail.com>
2026-01-10 22:22:50 +0700

    feat: add rotate and entrypoints

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'KASA')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(entrypoints: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/faq.js'])
 </head>
 <body class="bg-gray-900 text-white min-h-screen">
 
